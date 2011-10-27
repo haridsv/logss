9c235c8f5ab0eb5a91e4e904369fafc5c20af626
Hari Dara <haridara@gmail.com>
2011-10-26 20:55:36 -0700

    Cleaner way to list the ids, verified the existing functionality

--- logss.py
+++ logss.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# References:
+# - Column tags: https://groups.google.com/d/msg/google-spreadsheets-api/dZvMNrfcfQU/OyXBWEZyKdwJ
+# - GSX: http://code.google.com/apis/spreadsheets/data/3.0/reference.html#gsx_reference
 
 """Log a row to a Google Spreadsheet."""
 
@@ -14,6 +17,7 @@ import sys
 
 import gdata.gauth
 import gdata.spreadsheets.client
+import gdata.spreadsheets.data
 
 import oneshot
 
@@ -134,19 +138,12 @@ class MySpreadsheetsClient(gdata.spreadsheets.client.SpreadsheetsClient):
 
   GetListFeed = get_list_feed
 
-  def insert_row(self, data, key, wksht_id='default'):
-    new_entry = MyListEntry()
-    for k, v in data.iteritems():
-      new_entry.set_value(k, v)
-    return self.post(new_entry, self.LISTS_URL % (key, wksht_id))
-
-  InsertRow = insert_row
-
 class LogssAction(object):
 
   def __init__(self, debug=False):
     self.debug = debug
     self.client = MySpreadsheetsClient()
+    self.client.debug = debug
     self.client.http_client.debug = debug
     self.client.source = os.path.basename(sys.argv[0])
 
@@ -154,57 +151,39 @@ class LogssAction(object):
     client_authz = ClientAuthorizer(logger=logger)
     client_authz.EnsureAuthToken(self.client)
 
-  def GetKeys(self, ss=None, ss_is_id=False, ws=None, ws_is_id=False, lookup_names=False):
+  def GetSpreadsheets(self, ss=None, ss_is_id=False):
     """
-    Returns two structures. The first one is a map of spreadsheet ids keyed by
-    their names and the second one is a map of maps of worksheet ids first keyed
-    by their worksheet names and then by their spreadsheet ids.
-
-    If an id is passed for spreadsheet, it will not lookup a name, unless
-    lookup_names is True.
+    Return a generator of spreadsheet (name, id) pairs.
+    Given a spreadsheet name or ID, entry for only that spreadsheet is
+    generated.
     """
-    spKeysByName = None
-    if not ss or not ss_is_id or (ss_is_id and lookup_names):
-      # Get all spreadsheets.
-      spreadsheets = self.client.GetSpreadsheets()
-      spKeysByName = self._create_name_id_map(spreadsheets.entry, ss, ss_is_id)
-    elif ss and (ss_is_id and not lookup_names):
-      spKeysByName = {ss: ss}
-
-    # Get all the worksheets.
-    if spKeysByName:
-      wsKeysMap = dict()
-      for key in spKeysByName.values():
-        if not ws or not ws_is_id or (ws_is_id and lookup_names):
-          worksheets = self.client.GetWorksheets(key)
-          wsKeysByName = self._create_name_id_map(worksheets.entry, ws, ws_is_id)
-        elif ws and (ws_is_id and not lookup_names):
-          wsKeysByName = {ws: ws}
-        wsKeysMap[key] = wsKeysByName
-
-      return spKeysByName, wsKeysMap
-
-  def _create_name_id_map(self, entries, tok, tok_is_id):
-    mapByName = self._gen_id_by_name_map(entries)
-    if tok:
-      name = None
-      key = None
-      if not tok_is_id:
-        name = tok
-        key = mapByName.get(tok, None)
+    # Get all spreadsheets.
+    spreadsheets = self.client.GetSpreadsheets()
+    for (ssname, ssid) in self._gen_name_id(spreadsheets.entry):
+      if ss:
+        if (ss_is_id and ss == ssid) or (ssname == ss):
+          yield ssname, ssid
       else:
-        entry = [name for (name, key) in mapByName.items() if key == tok]
-        if entry:
-          # Expect only one entry to match, if not it is serious problem with gdata API.
-          name = entry[0]
-          key = tok
-      mapByName = {name: key}
-    return mapByName
+        yield ssname, ssid
 
-  def _gen_id_by_name_map(self, entries):
-    return dict([(entry.title.text, entry.id.text.split('/')[-1])
-                 for entry in entries])
+  def GetWorksheets(self, ssid, ws=None, ws_is_id=False):
+    """
+    Return a generator of worksheet (nanme, id) pairs for the specified
+    spreadsheet..
+    Given a worksheet name or id, only the entry for that worksheet is
+    generated.
+    """
+    worksheets = self.client.GetWorksheets(ssid)
+    for (wsname, wsid) in self._gen_name_id(worksheets.entry):
+      if ws:
+        if (ws_is_id and ws == wsid) or (wsname == ws):
+          yield wsname, wsid
+      else:
+        yield wsname, wsid
 
+  def _gen_name_id(self, entries):
+    for entry in entries:
+      yield entry.title.text, entry.id.text.split('/')[-1]
 
 class SpreadsheetInserter(LogssAction):
   """A utility to insert rows into a spreadsheet."""
@@ -219,7 +198,9 @@ class SpreadsheetInserter(LogssAction):
     return len([c for c in cols if ':' in c]) > 0
 
   def InsertRow(self, data):
-    self.client.InsertRow(data, self.key, wksht_id=self.wkey)
+    row_entry = gdata.spreadsheets.data.ListEntry()
+    row_entry.from_dict(data)
+    self.client.add_list_entry(row_entry, self.key, self.wkey)
 
   def InsertFromColumns(self, cols):
     # Data is mixed into column names.
@@ -242,14 +223,15 @@ def DefineFlags():
   desc = """
 Log data into a Google Spreadsheet.
 
-With no further arguments, a list of column names will be printed to stdout.
+With no further arguments, a list of column tags will be printed to stdout.
+These are typically column names in lower case with spaces and special
+characters stripped.
 
-Otherwise, remaining arguments should be of the form `columnname:value'.
+Otherwise, remaining arguments should be of the form `columntag:value'.
 One row will be added for each invocation of this program.
 
-If you just specify column names (without a value), then data will be read
-from stdin in whitespace delimited form, and mapped to each column name
-in order.
+If you just specify column tags (without a value), then data will be read
+from stdin in whitespace delimited form, and mapped to each column in order.
   """
   parser = optparse.OptionParser(usage=usage, description=desc)
   parser.add_option('--debug', dest='debug', action='store_true',
@@ -261,8 +243,7 @@ in order.
   parser.add_option('--name', dest='ssname',
                     help='The name of the spreadsheet to update or list')
   parser.add_option('--sheet', dest='wsname',
-                    help='The name of the worksheet to update',
-                    default='default'),
+                    help='The name of the worksheet to update (defaults to the first one)')
   parser.add_option('--list', dest='listkeys', action='store_true',
                     help='Lists the id of the specified sheet (by --name and --sheet) or all sheets in the specified spreadsheet (by --name) or all availale sheets')
   return parser
@@ -281,33 +262,27 @@ def main():
   if not opts.listkeys:
     if (not opts.ssname and not opts.ssid):
       parser.error('You must specify either --name or --key options')
-    if (not opts.wsname and not opts.wsid):
-      parser.error('You must specify either --sheet or --sheetid options')
 
   if opts.listkeys:
     lister = LogssAction(debug=opts.debug)
     lister.Authenticate()
-    spKeysByName, wsKeysMap = lister.GetKeys(opts.ssid or opts.ssname,
-                                             not opts.ssname,
-                                             opts.wsid or opts.wsname,
-                                             not opts.wsname,
-                                             True)
-    for (ssname, ssid) in spKeysByName.items():
+    for (ssname, ssid) in lister.GetSpreadsheets(opts.ssid or opts.ssname,
+                                                 not opts.ssname):
       print "%s: %s" % (ssname, ssid)
-      for (wsname, wsid) in wsKeysMap[ssid].items():
+      for (wsname, wsid) in lister.GetWorksheets(ssid,
+                                                 opts.wsid or opts.wsname,
+                                                 not opts.wsname):
         print "\t%s: %s" % (wsname, wsid)
   else:
     inserter = SpreadsheetInserter(debug=opts.debug)
     inserter.Authenticate()
-    if not opts.ssid or not opts.wsid:
-      spKeysByName, wsKeysMap = inserter.GetKeys(opts.ssid or opts.ssname,
-                                                 not opts.ssname,
-                                                 opts.wsid or opts.wsname,
-                                                 not opts.wsname)
-      opts.ssid = spKeysByName.keys()[0]
-      opts.wsid = wsKeysMap[opts.ssid].keys()[0]
-    inserter.key = opts.ssid
-    inserter.wkey = opts.wsid
+    ssid = opts.ssid or list(inserter.GetSpreadsheets(opts.ssname))[0][1]
+    wsid = (opts.wsid or
+            (opts.wsname and
+             list(inserter.GetWorksheets(ssid, opts.wsname))[0][1]) or
+            'default')
+    inserter.key = ssid
+    inserter.wkey = wsid
 
     if len(args) > 1:
       cols = args
@@ -318,7 +293,7 @@ def main():
         inserter.InsertFromFileHandle(cols, sys.stdin)
     else:
       print('\n'.join(inserter.ListColumns()))
-    return 0
+  return 0
 
 
 if __name__ == '__main__':
